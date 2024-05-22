f5c26a761b85159f817f3cabd040b4a9eace5c51
Tanka Magar <tankastark@gmail.com>
2024-05-22 17:38:03 +0545

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+const MyComponent = () => {
+  return (
+    <div>
+      <p>This is Tanka.EXE</p>
+      <div>
+        <button>Button 1</button>
+        <button>Button 2</button>
+      </div>
+      <div>
+        <a href="/Resume.pdf" download="MyCV.pdf">
+          <button>Download CV</button>
+        </a>
+        <a href="/tanka2017.png" download="tanka2017.png">
+          <button>Download Image</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
